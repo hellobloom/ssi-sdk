7717eb67acabd15b803e39ddeb725185e7ab867b
JaceHensley <jacehensley@gmail.com>
2021-06-18 12:06:24 -0700

    Add keyUtils (#15)

--- packages/ecdsa-secp256k1-verification-key-2019/CHANGELOG.md
+++ packages/ecdsa-secp256k1-verification-key-2019/CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.1.2
+
+- Add `keyUtils` which has functions to convert to and from hex, base68, jwk, and Uint8Array formatted keys
+- Improve `EcdsaSecp256k1VerificationKey2019.generate` to not infinitely loop if provided seed is invalid
+
 ## 0.1.1
 
 - Switch to `yarn` and add `resolutions` to package.json for some build dependencies
--- packages/ecdsa-secp256k1-verification-key-2019/src/__tests__/__fixtures__/index.ts
+++ packages/ecdsa-secp256k1-verification-key-2019/src/__tests__/__fixtures__/index.ts
@@ -1,3 +1,44 @@
+export const keyConfig = {
+  privateKeyHex: '42bbd1cc99e74decde73a603895f3402c79a730a27fc93f294d58f948b67bcfa',
+  publicKeyHex: '031e1c387aef9e6321b1585b80c6086b76a9d084fc8d36d5c29e2fff290d737a29',
+
+  privateKeyBase58: '5VW1xpufuLGiqRB1cDPvSSxku5JP3kwoJAArNEFzDEvu',
+  publicKeyBase58: 'viVJUVxmaviV1VFyHvnCUbf95eiD6G2qrqeG74xsLwV2',
+
+  privateKeyJWK: {
+    kty: 'EC',
+    crv: 'secp256k1',
+    d: 'QrvRzJnnTezec6YDiV80Aseacwon_JPylNWPlItnvPo',
+    x: 'Hhw4eu-eYyGxWFuAxghrdqnQhPyNNtXCni__KQ1zeik',
+    y: 'fnmECouL1f2Gi4Bj6BQpYSuTHWIinddmeEqEz3fplkk',
+    kid: 'kid',
+  },
+  publicKeyJWK: {
+    kty: 'EC',
+    crv: 'secp256k1',
+    x: 'Hhw4eu-eYyGxWFuAxghrdqnQhPyNNtXCni__KQ1zeik',
+    y: 'fnmECouL1f2Gi4Bj6BQpYSuTHWIinddmeEqEz3fplkk',
+    kid: 'kid',
+  },
+
+  // prettier-ignore
+  privateKeyUint8Array: new Uint8Array([
+     66, 187, 209, 204, 153, 231,  77,
+    236, 222, 115, 166,   3, 137,  95,
+     52,   2, 199, 154, 115,  10,  39,
+    252, 147, 242, 148, 213, 143, 148,
+    139, 103, 188, 250,
+  ]),
+  // prettier-ignore
+  publicKeyUint8Array: new Uint8Array([
+     3,  30,  28,  56, 122, 239, 158,  99,
+    33, 177,  88,  91, 128, 198,   8, 107,
+   118, 169, 208, 132, 252, 141,  54, 213,
+   194, 158,  47, 255,  41,  13, 115, 122,
+    41,
+  ]),
+}
+
 export const document = {
   '@context': ['http://schema.org', 'https://ns.did.ai/suites/secp256k1-2019/v1'],
   '@type': 'Person',
